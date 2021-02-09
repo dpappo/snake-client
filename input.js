6579a1272de399e80b08cc3e41d889e898f5a4ef
Dan Pappo <dpappo@gmail.com>
2021-02-09 14:17:56 -0500

    separated input into separate file

---
+++ input.js
@@ -0,0 +1,20 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function () {
+    stdin.on("data", (key) => {
+      process.stdout.write(key);
+      if (key === "\u0003") {
+        process.exit();
+      }
+    });
+  };
+  handleUserInput();
+
+  return stdin;
+};
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,28 +1,11 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 console.log("Connecting ...");
+connect();
 
 /**
  * Setup User Interface
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  const handleUserInput = function () {
-    stdin.on("data", (key) => {
-      process.stdout.write(key);
-      if (key === "\u0003") {
-        process.exit();
-      }
-    });
-  };
-  handleUserInput();
-
-  return stdin;
-};
 
 setupInput();
-connect();
