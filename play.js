f910a344907cd6de490c1ef9c9b59b48a2b2bc0b
Dan Pappo <dpappo@gmail.com>
2021-02-09 14:13:36 -0500

    added handler for input, including escape with ctrl+C

--- play.js
+++ play.js
@@ -1,3 +1,28 @@
 const { connect } = require("./client");
 console.log("Connecting ...");
+
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function () {
+    stdin.on("data", (key) => {
+      process.stdout.write(key);
+      if (key === "\u0003") {
+        process.exit();
+      }
+    });
+  };
+  handleUserInput();
+
+  return stdin;
+};
+
+setupInput();
 connect();
