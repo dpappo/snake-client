eb3e144cb9c886f4cec6c21f0b4e4d461b6e25fc
Dan Pappo <dpappo@gmail.com>
2021-02-09 09:29:01 -0500

    experimented with commented out move command

--- client.js
+++ client.js
@@ -12,6 +12,9 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Successfully connected to game server ");
     conn.write("Name: DP");
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 100);
   });
 
   conn.on("data", (data) => {
